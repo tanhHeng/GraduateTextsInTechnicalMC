77f812c6bce7b2c006012b1202f8390b626b2d47
xhbsh <qq260342855@gmail.com>
2024-12-29 17:23:16 +0800

    自定义字体调整 * 显示当前字体大小 * bug fix 将同时改变标题和代码框中的字体大小

--- source/js/fontsize.js
+++ source/js/fontsize.js
@@ -9,16 +9,23 @@ function adjustFontSize(action) {
     }
 
     markdownSection.style.setProperty('--font-size', `${currentSize}px`);
-
+    updateFontSizeDisplay(currentSize);
     localStorage.setItem('font-size', currentSize);
 }
 
 function applyInitialStyles() {
-    // 从 localStorage 加载保存的字体大小
     const savedSize = localStorage.getItem('font-size');
     if (savedSize) {
         const markdownSection = document.querySelector('.markdown-section#main');
         markdownSection.style.setProperty('--font-size', `${savedSize}px`);
+        updateFontSizeDisplay(savedSize);
+    }
+}
+
+function updateFontSizeDisplay(fontSize) {
+    const fontSizeDisplay = document.querySelector('.font-size-display');
+    if (fontSizeDisplay) {
+        fontSizeDisplay.textContent = `当前字体大小: ${fontSize}px`;
     }
 }
 
@@ -55,14 +62,40 @@ function styleInject(css, ref) {
 
 var css = `
 .markdown-section {
-    --font-size: 15px; 
+    --font-size: 15px;
     font-size: var(--font-size);
 }
 
-.markdown-section ol,
-.markdown-section p,
-.markdown-section ul {
-    line-height: calc(var(--font-size) * 2.1);
+.markdown-section code {
+    font-size: calc(var(--font-size) - 2px) !important;
+}
+
+.markdown-section ol, .markdown-section p, .markdown-section ul {
+    line-height: calc(var(--font-size) * 2);
+}
+
+.markdown-section h1 {
+    font-size: calc(var(--font-size) * 2.3);
+}
+
+.markdown-section h2 {
+    font-size: calc(var(--font-size) * 2.1);
+}
+
+.markdown-section h3 {
+    font-size: calc(var(--font-size) * 1.8);
+}
+
+.markdown-section h4 {
+    font-size: calc(var(--font-size) * 1.5);
+}
+
+.markdown-section h5 {
+    font-size: calc(var(--font-size) * 1.3);
+}
+
+.markdown-section h6 {
+    font-size: calc(var(--font-size) * 1.1);
 }
 
 button.font-size-button {
@@ -70,7 +103,7 @@ button.font-size-button {
     top: 15px;
     left: 55px;
     padding: 6px;
-    background-color: #fff;
+    background: transparent;
     cursor: pointer;
     z-index: 1000;
     font-size: 16px;
@@ -79,11 +112,24 @@ button.font-size-button {
 }
 
 button.font-size-button[onclick="adjustFontSize('decrease')"] {
-    left: 100px; 
+    left: 100px;
 }
 
 .docsify-dark-mode button.font-size-button {
-    background-color: var(--dark-base-background);
+    color: var(--dark-base-color);
+}
+
+.font-size-display {
+    position: absolute;
+    top: 17px;
+    left: 139px;
+    background: transparent;
+    font-size: 16px !important;
+    z-index: 1000;
+    color: #333;
+}
+
+.docsify-dark-mode .font-size-display{
     color: var(--dark-base-color);
 }
 `;
@@ -91,10 +137,12 @@ button.font-size-button[onclick="adjustFontSize('decrease')"] {
 styleInject(css);
 
 function install(hook, vm) {
+    const savedSize = localStorage.getItem('font-size');
     hook.afterEach(function (html) {
         var fontSizeButtons = `
             <button onclick="adjustFontSize('increase')" class="font-size-button" aria-label="Increase font size" title="Increase font size">A+</button>
             <button onclick="adjustFontSize('decrease')" class="font-size-button" aria-label="Decrease font size" title="Decrease font size">A-</button>
+            <div class="font-size-display">当前字体大小: ${savedSize}px</div>
         `;
         return fontSizeButtons + html;
     });
